f6d134ea5047896505a9bd0c67937fcaf25bd94a
itbdelaboprogramming <81901213+itbdelaboprogramming@users.noreply.github.com>
2021-06-05 00:33:22 +0900

    Add files via upload

---
+++ battery_monitoring_raspy.py
@@ -0,0 +1,74 @@
+"""
+#!/usr/bin/env python
+#title           :battery_monitoring_raspi.py
+#description     :-
+#author          :Fajar Muhammad Noor Rozaqi
+#date            :2021/04/15
+#version         :0.2
+#usage           :BMS-python
+#notes           :
+#python_version  :3.9
+#==============================================================================
+"""
+
+#library
+import time
+import datetime
+import serial
+import pymysql
+
+#serial tx/rx pins on the GPIO header or USB
+serial_port = '/dev/ttyUSB0'
+#it must be same rate used on the Arduino
+serial_rate = 9600
+
+#function
+def main():
+    ser = serial.Serial(serial_port, serial_rate)
+    ser.flushInput()
+    while True:
+        try:
+            reading = ser.readline()
+            reading = reading.decode("utf-8")
+            reading = reading.rstrip()
+            reading = reading.split(',')
+            print(reading)
+
+            #baterai 1
+            timer = datetime.datetime.now()
+            V_1 = str(reading[1])
+            T_1 = str(reading[-1])
+            
+            print("Time            :",timer.strftime("%Y-%m-%d %H:%M:%S"))
+            print("Voltage_1       :",V_1,"V")
+            print("Temperature_1   :",T_1,"C")
+            
+            #Database Connection
+            db = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='bmstest',
+                                 db='monitoring')
+            
+            cur = db.cursor()
+            
+            try:
+                add_c1 ="INSERT INTO `battery_monitoring`(time,voltage_1,temperature_1) VALUES(%s,%s,%s)"
+                cur.execute(add_c1,(timer.strftime("%Y-%m-%d %H:%M:%S"),
+                                    V_1,
+                                    T_1))
+                db.commit()
+                print("Data is sent")
+                print("")
+                
+            except:
+                db.rollback()
+                print("Database is not connected")
+                
+            time.sleep(5)
+            
+        except:
+            print("Raspberry Pi is NOT connected")
+            break
+
+if __name__ == "__main__":
+    main()
