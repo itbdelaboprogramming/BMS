d10a3663b1db429fb7d075baf4b1d8df85d15c19
fajarmnrozaki <46889787+fajarmnrozaki@users.noreply.github.com>
2022-07-28 17:28:06 +0700

    Update CPU_RaspberryPI.py
    Adding database connection and converting the file database Mysql into CSV

--- test_raspberrypi/CPU_RaspberryPI.py
+++ test_raspberrypi/CPU_RaspberryPI.py
@@ -2,6 +2,8 @@
 import time
 import datetime
 import pymysql
+import csv
+import numpy as np
 
 while True:
     try:
@@ -14,12 +16,13 @@ while True:
         print("Temperature Raspberry Pi:",cpu.temperature)
         print("")
         
-        
         #Database Connection
-        db = pymysql.connect(host='localhost',
-                             user='root',
-                             password='bmstest',
-                             db='monitoring')
+        db = pymysql.connect(host='192.168.18.32',
+                     user='admin',
+                     password='admin',
+                     db='monitoring',
+                     charset='utf8',
+                     cursorclass=pymysql.cursors.DictCursor)
         cur = db.cursor()
         
         try:
@@ -29,10 +32,52 @@ while True:
                                 int(1)))
             db.commit()
             
+            time.sleep(2)
+            
+            #Reading Record Database
+            try:
+                #Database Connection
+                db = pymysql.connect(host='192.168.18.32',
+                     user='admin',
+                     password='admin',
+                     db='monitoring',
+                     charset='utf8',
+                     cursorclass=pymysql.cursors.DictCursor)
+                db.cursor()
+                
+                with db.cursor() as cursor:
+                    sql = "SELECT (temperature),(time),(raspi_id) FROM `cpu_temperature`"
+                    cursor.execute(sql)
+                    result = cursor.fetchall()
+                    print(result)
+                 
+                Temperature =[]
+                Time =[]
+                Raspi_id =[]
+                 
+                #Converting into CSV File
+                filename = "/home/pi/temperaturelog.csv"
+                with open(filename, mode='w', newline='') as file:
+                    a = csv.writer(file, delimiter =',')
+                    a.writerow(['Temperature','Time','Raspi_id'])
+                    for i in range(0, len(result)):
+                        a.writerow(result[i].values())
+                        Temperature.append(result[i]['temperature'])
+                        Time.append(result[i]['time'])
+                        Raspi_id.append(result[i]['raspi_id'])
+                
+                print("CSV is converted")
+                print("")
+                
+            except:
+                print("CSV is not converted")
+                print("")
+                
         except:
             db.rollback()
             print("Database is not connected")
+            
+        time.sleep(0)
         
-        time.sleep(5)
     except:
         pass
