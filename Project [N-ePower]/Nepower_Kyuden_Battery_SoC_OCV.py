cce6bc6455d830752c989e240c9c95e436a9b1c6
fajarmnrozaki <zakifajar96@gmail.com>
2024-11-12 18:52:33 +0700

    Python program for real-time SoC-OCV (Open Circuit Voltage) Method

--- Project [N-ePower]/Nepower_Kyuden_Battery_SoC_OCV.py
+++ Project [N-ePower]/Nepower_Kyuden_Battery_SoC_OCV.py
@@ -1,148 +1,100 @@
-"""
 #!/usr/bin/env python
 #title           :Nepower_Kyuden_Battery_SoC_OCV.py
 #description     :SOC OCV (Open Circuit Voltage) method For Kyuden Battery.
 #author          :Fajar Muhammad Noor Rozaqi
-#date            :2023/06/05
-#version         :0.1
-#usage           :BMS-python
-#notes           :Program/Algorithm is under review
-#python_version  :3.8
+#date            :2024/11/01
+#version         :0.3
+#usage           :BMS-Python
+#notes           :
+#python_version  :3.9.7
 #==============================================================================
-"""
 
-#import library
-import numpy as np
+# Import libraries
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import time
-from datetime import datetime
-import csv
-# import seaborn as sns
-# sns.set(style="darkgrid")
 
-#start of program
-start = time.time()
+# Define coefficients for the polynomial
+POLYNOMIAL_COEFFICIENTS = {
+    'x11': 1.7738173870072556e-18,
+    'x10': -4.801327471977691e-15,
+    'x9': 5.560620735462417e-12,
+    'x8': -3.482397700973714e-09,
+    'x7': 1.1567625787124435e-06,
+    'x6': -8.933932249320343e-05,
+    'x5': -0.09253353703659696,
+    'x4': 45.253376176605954,
+    'x3': -10545.003853169965,
+    'x2': 1421098.0573940529,
+    'x1': -106515278.96156228,
+    'x0': 3458910194.4554205
+}
 
-#Algorithm of SOC Estimation Battery
-print("====+++++++++++++++++++++++++====")
-print("====+++++++++++++++++++++++++====")
-print("====+++++++++++++++++++++++++====")
-print("====Estimation of SOC Battery====")
-print("====+++++++++++++++++++++++++====")
-print("====+++++++++++++++++++++++++====")
-print("====+++++++++++++++++++++++++====")
+def evaluate_polynomial(v):
+    """
+    Evaluate the polynomial for SOC estimation given a voltage value.
+    Args:
+        v (float): Voltage value.
+    Returns:
+        float: Estimated SOC value.
+    """
+    coeffs = POLYNOMIAL_COEFFICIENTS
+    return (coeffs['x11'] * v**11 + coeffs['x10'] * v**10 + coeffs['x9'] * v**9 + 
+            coeffs['x8'] * v**8 + coeffs['x7'] * v**7 + coeffs['x6'] * v**6 + 
+            coeffs['x5'] * v**5 + coeffs['x4'] * v**4 + coeffs['x3'] * v**3 + 
+            coeffs['x2'] * v**2 + coeffs['x1'] * v + coeffs['x0'])
 
-#first array
-#sub of battery
-v_soc = []
-i_soc = []
-soc = []
+def read_data(file_path):
+    """
+    Read and preprocess data from an Excel file.
+    Args:
+        file_path (str): Path to the Excel file.
+    Returns:
+        tuple: Tuple containing time and voltage series.
+    """
+    data = pd.read_excel(file_path)
+    time = pd.Series(np.ravel(data.iloc[:, 0])).dropna()  # Column A for time
+    voltage = pd.Series(np.ravel(data.iloc[:, 2])).dropna()  # Column C for voltage
+    min_length = min(len(time), len(voltage))
+    return time[:min_length], voltage[:min_length]
 
-#sub of time
-time_soc = []
+def estimate_soc(voltage_series):
+    """
+    Estimate SOC for each voltage value in the series.
+    Args:
+        voltage_series (pd.Series): Series of voltage values.
+    Returns:
+        pd.Series: Estimated SOC values.
+    """
+    return pd.Series([evaluate_polynomial(v) for v in voltage_series]).dropna()
 
-#Reading of Battery
-# with open('D:/mppt_4f_six.csv', newline='') as f:
-#     reader = csv.reader(f)
-#     for row in reader:
-#         print(row)
-data_soc = pd.read_csv('D:/BMS_test1.csv')
+def plot_soc_estimation(time_series, soc_series):
+    """
+    Plot the SOC estimation over time.
+    Args:
+        time_series (pd.Series): Series of time values.
+        soc_series (pd.Series): Series of estimated SOC values.
+    """
+    plt.figure(figsize=(10, 5))
+    plt.plot(time_series, soc_series, label='SOC Estimate (OCV Method)', color='blue', lw=0.75)
+    plt.xlabel('Time')
+    plt.ylabel('SOC (%)')
+    plt.title('SOC Estimation Over Time')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-#initiation of data reading
-v_soc = data_soc['BatVolt']
-v_soc = pd.Series(np.ravel(v_soc)).dropna()
-i_soc = data_soc['BatCur']
-i_soc = pd.Series(np.ravel(i_soc)).dropna()
-time_soc = data_soc['time']
-time_soc = pd.to_datetime(np.ravel(time_soc)).dropna()
-# time_soc = pd.Series(np.ravel(time_soc)).dropna()
+def main():
+    """
+    Main function to execute the SOC estimation process.
+    """
+    print("==== Estimation of SOC Battery ====")
+    file_path = 'D:/discharging2_filtered.xlsx'
+    time_series, voltage_series = read_data(file_path)
+    soc_series = estimate_soc(voltage_series)
+    print("Estimated SOC values:")
+    print(soc_series)
+    plot_soc_estimation(time_series, soc_series)
 
-##plotting V,I,T based on reading
-# plt.style.use('dark_background')
-# figure, ax1 = plt.subplots(figsize=(9.7,4.5))
-# color = 'orange'
-# ax1.set_xlabel('time (yyyy-mm-ddThh:mm:ssZ)', fontsize=12)
-# ax1.set_ylabel('Battery Voltage (V)',fontsize=12)
-# ax1.plot(time_0, v_0,label='Battery Voltage (V)', color=color, lw=0.5)
-# ax1.tick_params(axis='y')
-# # axes = plt.gca()
-# # axes.set_ylim([25,34])
-# ax1.legend(loc='lower left')
-# ax1.grid()
-
-# ax2 = ax1.twinx()
-# color = 'blue'
-# ax2.set_ylabel('Battery Current (A)',fontsize=12)
-# ax2.plot(time_0, i_0,label='Battery Current (A)', color=color, lw=0.5)
-# ax2.tick_params(axis='y')
-# # axes = plt.gca()
-# # axes.set_ylim([-15,15])
-# figure.tight_layout()
-# ax2.legend(loc='lower right')
-# plt.figure(1).savefig('Test 1', quality=100)
-# # plt.style.use('dark_background')
-
-#Estimation of SOC Battery
-def SOC_estimation():
-
-	# #Initial value
-	# capacity = 116*2
-	# SOC = 50 #initial soc had to be tuned first
-	# dt = 1/60
-
-	# #Estimation SOC with CC Method
-	# SOC_cc = []
-	# for i, data in enumerate(i_0):
-	# 	# I = i_0[i]
-	# 	SOC = SOC + ((i_0[i]*dt)/capacity)
-	# 	SOC_cc.append(SOC)
-	# SOC_cc = pd.Series(np.ravel(SOC_cc)).dropna()
-	# print(SOC_cc)
-
-	#Estimation Soc with OCV 7th order
-	SOC_ocv = []
-	soc = []
-
-	for i, data in enumerate(v_soc):
-		x8  	= 0
-		x7	= 0
-		x6	= 2.237762467861503
-		x5	= 0.03418556060441675
-		x4	= -4.243089817994437
-		x3	= 209.72587338311544
-		x2	= -5160.391592686133
-		x1	= 63224.4109174412
-		x0	= -308656.73942473787
-		
-		v = v_soc[i]
-		soc = (((v**8)*0)+((v**7)*0)+((v**6)*0)+((v**5)*x5)+((v**4)*x4)+((v**3)*x3)+((v**2)*x2)+((v**1)*x1)+((v**0)*x0))
-		SOC_ocv.append(soc)
-	SOC_ocv = pd.Series(np.ravel(SOC_ocv)).dropna()
-	print(SOC_ocv)
-	
-	# # Plotting
-	# cm = plt.cm.get_cmap('jet_r')
-
-	# plt.figure(figsize=(11,5))
-	# graph = plt.scatter(time_soc, SOC_ocv,
-	# 				 lw=0.1, c=SOC_ocv,
-	# 				 alpha=0.5, label="SOC_ocv",
-	# 				 cmap=cm,vmin=0, vmax=100, s=10)
-	# plt.plot(time_soc, SOC_ocv,lw=0.5, c='black',alpha=0.25)
-	# plt.colorbar(graph)
-	# # plt.avxspan(pd.to_datetime())
-	# plt.xlabel("time (yyyy-mm-ddThh:mm:ssZ)", fontsize=12)
-	# plt.ylabel("SOC Battery(%)", fontsize=12)
-	# plt.legend(loc="lower right")
-	# # plt.xlim(42,50)
-	# # plt.ylim(0,100)
-	# plt.figure(1).savefig('SOC', quality=100)
-	# plt.grid(True)
-	# # Rotating the values along x-axis to 45 degrees
-	# # plt.xticks(rotation=15)
-	# # plt.tight_layout()	
-
-	return ()
-
-SOC_estimation = SOC_estimation()
+if __name__ == '__main__':
+    main()
